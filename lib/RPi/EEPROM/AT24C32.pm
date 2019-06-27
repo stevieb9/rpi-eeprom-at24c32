1ebd7d93eb20753709bf84ee143609ca96239212
Steve Bertrand <steveb@cpan.org>
2019-06-27 13:17:48 -0700

    initial commit of base skeleton

---
+++ lib/RPi/EEPROM/AT24C32.pm
@@ -0,0 +1,111 @@
+package RPi::EEPROM::AT24C32;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RPi::EEPROM::AT24C32 - The great new RPi::EEPROM::AT24C32!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RPi::EEPROM::AT24C32;
+
+    my $foo = RPi::EEPROM::AT24C32->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rpi-eeprom-at24c32 at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RPi-EEPROM-AT24C32>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RPi::EEPROM::AT24C32
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RPi-EEPROM-AT24C32>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RPi-EEPROM-AT24C32>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RPi-EEPROM-AT24C32>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RPi-EEPROM-AT24C32/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2019 Steve Bertrand.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of RPi::EEPROM::AT24C32
